78d8bc25024d2dab64238e5f7db5ae25bb4f2dd9
Priyanshi Sahu <priyanshi.sahu@iicstechnologies.com>
2024-11-10 12:23:57 +0530

    first commit

---
+++ book-network/src/main/java/com/alibou/book/BookNetworkApiApplication.java
@@ -0,0 +1,29 @@
+package com.alibou.book;
+
+import com.alibou.book.role.Role;
+import com.alibou.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAsync
+public class BookNetworkApiApplication {
+//jgne buga yvbx aqgj
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkApiApplication.class, args);
+    }
+//http://localhost:8088/api/v1/swagger-ui/index.html#/
+    @Bean
+    public CommandLineRunner runner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName("USER").isEmpty()) {
+                roleRepository.save(Role.builder().name("USER").build());
+            }
+        };
+    }
+}
